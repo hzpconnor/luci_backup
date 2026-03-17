a4f0043510cfd0b28677cd34ed8b9c6499e5fb85
connor <connorhzp@qq.com>
2026-03-17 10:38:56 +0800

    Update useradd.sh to set plaintext password directly and comment out user creation logic

--- YF-Router/useradd.sh
+++ YF-Router/useradd.sh
@@ -5,7 +5,9 @@
 # 你可以在你的 yf.sh 脚本里加上这段：
 uci add rpcd login
 uci set rpcd.@login[-1].username='user'
-uci set rpcd.@login[-1].password='$p$user'  # '$p$user'代表读取系统的/etc/shadow中的user密码，也可以直接写明文(不安全)
+# 这里的密码不再使用 '$p$user'（不再关联底层 /etc/shadow），而是直接设置明文或哈希字符串。
+# 这样系统就不会去校验系统底层是否存在该 Linux 用户，仅做 rpcd 级访问权限控制。
+uci set rpcd.@login[-1].password='123456'
 uci add_list rpcd.@login[-1].read='user_read'
 uci add_list rpcd.@login[-1].write='user_write'
 uci commit rpcd
@@ -14,7 +16,9 @@ uci commit rpcd
 
 # 同时，记得在底层系统给 user 设置密码（可以通过在 yf.sh 脚本中写入）：
 # 添加系统影子用户 (前提是没有此用户)
-echo "user:x:1001:1001:user:/var:/bin/false" >> /etc/passwd
-echo "user:!:19000:0:99999:7:::" >> /etc/shadow
+# if ! grep -q "^user:" /etc/passwd; then
+#     # 优先尝试 useradd，如果失败则尝试 busybox 的 adduser
+#     useradd -M -s /bin/false user 2>/dev/null || adduser -h /var -s /bin/false -D user 2>/dev/null
+# fi
 # 强制将 user 的密码设为 123456
-echo -e "123456\n123456" | passwd user
+# echo -e "123456\n123456" | passwd user
